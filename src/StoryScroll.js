1193fb03176ac3eeafc04de4b3a61b9e073f5580
 小玲欢 <0@eexx.me>
2019-09-12 01:16:18 +0800

    add pixi-projection

--- src/StoryScroll.js
+++ src/StoryScroll.js
@@ -1,8 +1,10 @@
-import * as PIXI from './pixi.js';
+import * as PIXI from 'pixi.js';
 import * as browser from "./browser";
 import {TweenMax} from "gsap/TweenMax";
 import Scroller from './Scroller';
 
+global.PIXI = PIXI;
+require("pixi-projection");
 
 class StoryScroll {
 	designWidth;
