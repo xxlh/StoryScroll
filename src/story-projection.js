8f99b4cdfe2f6eaa8319ac2a297f3a648e9dbf7f
 小玲欢 <0@eexx.me>
2019-09-15 01:32:40 +0800

    add chapter2d sprite2d

--- src/story-projection.js
+++ src/story-projection.js
@@ -4,10 +4,27 @@ global.PIXI = PIXI;
 require("pixi-projection");
 
 export default (() => {
-	StoryScroll.prototype.sprite2d = (imgsrc, o) => {
-		console.log('sprite2d111');
-
+	StoryScroll.prototype.chapter2d = function(o, _parent) {
+        let chapter2d = new PIXI.projection.Container2d();
+        chapter2d.sprite2d = (imgsrc) => this.sprite2d(imgsrc, o, chapter2d);
+		this._setProps(chapter2d, o);
+        this._setActions(chapter2d);
+		if (_parent) _parent.addChild(chapter2d);
+		else this.containerScroll.addChild(chapter2d);
+        return chapter2d; 
 	}
-
+	
+	StoryScroll.prototype.sprite2d = function(imgsrc, o, _parent) {
+		const sprite2d = new PIXI.projection.Sprite2d(PIXI.Texture.from(imgsrc));
+		sprite2d.proj.affine = PIXI.projection.AFFINE[typeof o.affine=='string'?o.affine.toUpperCase():'NONE'];
+		delete o.affine;
+		if (o.x && o.y) { sprite2d.position.set(o.x, o.y); delete o.x; delete o.y }
+		this._setProps(sprite2d, o);
+        this._setActions(sprite2d);
+		if (_parent) _parent.addChild(sprite2d);
+		else this.containerScroll.addChild(sprite2d);
+        return sprite2d;
+	}
+	
 	return global.PIXI.projection;
 })()
